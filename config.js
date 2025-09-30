49768ba171df92bc5597a1a3d0876617ce1c2126
FazzcodeX <fazzmodssdev@gmail.com>
2025-09-30 19:57:12 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  tokens: "8314349944:AAEp0m5z6DtRT-BieSPWkx3D2Wyz8yTZPXM",
+  tokens: "8257216349:AAFKVaVZ5tizFcihaP6IVfwgMOhEil9FLDk",
   owner: "8243394905",
   port: "2065",
   ipvps: "http://hanzzoffcx.privateserverarya.web.id"
